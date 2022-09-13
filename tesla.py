72c14ee5f30e861847e386c45c6338865a2241af
Michael Ababio <michael.ababio@contractors.mailchimp.com>
2022-09-13 15:52:35 -0400

    add tesla.py to dockerfile

--- tesla.py
+++ tesla.py
@@ -4,15 +4,27 @@ import requests
 class Tesla:
 
     def __init__(self):
-        
-        
+        hold = 1
+
+    def set_temp(self,temp):
+        return True
+
+
+    def is_tesla_ready_for_climate_on(self):
+        return True
+
+
+    def is_dog_in_car(self):
+        return True
+
+
     def is_tesla_moving(self):
         speed = requests.post(self.url_tesla_location).json()['speed']
         if speed == 'None':
             return False
         else:
             return True
-    
+
     @retry(delay=2, tries=3)
     def is_close(self):
         return_value = self.get_full_proximity()
@@ -21,7 +33,7 @@ class Tesla:
             return True
         else:
             return False
-    
+
     @retry(delay=2, tries=3)
     def get_full_proximity(self):
         r_location = requests.post(self.url_tesla_location)
@@ -29,7 +41,7 @@ class Tesla:
         lon = float(r_location.json()['lon'])
         param_prox = {"lat": lat, "lon": lon}
         return requests.post(self.url_tesla_prox, json=param_prox).json()
-    
+
     @retry(delay=2, tries=3)
     def get_location(self):
         r_location = requests.post(self.url_tesla_location)
@@ -37,7 +49,7 @@ class Tesla:
         lon = float(r_location.json()['lon'])
         param_prox = {"lat": lat, "lon": lon}
         return param_prox
-    
+
     def is_on_home_street(self):
         latlon = self.get_location()
         reverse_geocode_result = self.gmaps.reverse_geocode((latlon['lat'], latlon['lon']))

9c8c1d7cb0e31281bd7eb6f64c2ac6aed6a1c97d
Michael Ababio <michaelkwasi@gmail.com>
2022-09-14 13:30:20 -0400

    moved tap class into app folder

--- dockerfile
+++ dockerfile
@@ -4,13 +4,9 @@ ENV APP_HOME /app
 WORKDIR $APP_HOME
 COPY app  app/
 COPY main.py ./
-# Copy tesla.py ./
-# COPY db_mongo.py ./
-# COPY sms.py ./
 COPY requirement.txt ./
 RUN pip install --no-cache-dir -r  requirement.txt
 RUN pip install 'pymongo[srv]'
 ENV PORT 8080
-#ENV IFTTT_TRIGGER_LOCK False
 # Timeout is set to 0 to disable the timeouts of the workers to allow Cloud Run to handle instance scaling.
 CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 main:app
--- main.py
+++ main.py
@@ -1,95 +1,10 @@
-import requests
-import time
 from app import sms
 import os
 from flask import Flask
 from flask import g
 from flask_executor import Executor
 from app.db_mongo import db_client
-from app.tesla import Tesla
-
-
-class TAP:
-
-    def __init__(self):
-        self.garage_still_open = None
-        self.stil_on_home_street = None
-        self.garage_open_limit = 20  # 5mins
-        self.confirmation_limit = 20
-        self.db = db_client()
-        self.tesla_obj = Tesla()
-
-    def garage_isopen(self):
-        url_myq_garage = "https://us-east4-ensure-dev-zone.cloudfunctions.net/function-trigger-myq"
-        param = {"isopen": ''}
-        if requests.post(url_myq_garage, json=param).json()['isopen'] == 'closed':
-            return False
-        else:
-            return True
-
-    def confirmation_before_armed(self):
-        while self.garage_isopen() and not self.garage_open_limit == 0:
-            sms.send_sms('Checking if still open for 5 mins' + str(self.garage_open_limit))
-            time.sleep(5)
-            self.garage_open_limit -= 5
-        else:
-            if not self.garage_isopen() and self.tesla_obj.is_tesla_moving() and not self.tesla_obj.is_on_home_street():
-                sms.send_sms('garage is closed and car is moving and not on home street')
-                return True
-            elif self.garage_isopen() and self.garage_open_limit == 0:
-                sms.send_sms('garage has been open for more than 5 mins and we are terminating confirmation function')
-                self.garage_still_open = True
-                return False
-            else:
-                while self.tesla_obj.is_on_home_street() and not self.confirmation_limit == 0:
-                    sms.send_sms('car is parked on street with garage closed')
-                    time.sleep(5)
-                    self.confirmation_limit -= 5
-                else:
-                    if not self.tesla_obj.is_on_home_street():  # not on home street
-                        sms.send_sms('Not sure about this case, but returning true')
-                        return True
-                    else:
-                        self.stil_on_home_street = True
-                        return False
-
-
-
-    def garage(self, state):
-        url_myq_garage = "https://us-east4-ensure-dev-zone.cloudfunctions.net/function-trigger-myq"
-        param = {"state": state}
-        return requests.post(url_myq_garage, json=param).json()
-
-    def trigger_tesla_home_automation(self):
-        self.garage('open')
-        sms.send_sms('Garage door opening!')
-
-    def cleanup(self):
-        sms.send_sms('Setinng job done')
-        self.db.set_door_close_status("came_home")
-
-    def tesla_home_automation_engine(self):
-        while not self.tesla_obj.is_close():
-            if self.tesla_obj.proximity_value < .07:
-                continue
-            elif self.tesla_obj.proximity_value < 1:
-                sms.send_sms("Delay for 1 secs")
-                time.sleep(1)
-            elif self.tesla_obj.proximity_value < 2:
-                sms.send_sms("Delay for 15 sec")
-                time.sleep(15)
-            elif self.tesla_obj.proximity_value < 3:
-                sms.send_sms("Delay for 2 mins")
-                time.sleep(120)
-            elif self.tesla_obj.proximity_value < 7:
-                sms.send_sms("Delay for 5 mins")
-                time.sleep(300)
-            else:
-                sms.send_sms("Delay for 15 mins")
-                time.sleep(900)
-        else:
-            self.trigger_tesla_home_automation()
-
+from app.tap import TAP
 
 app = Flask(__name__)
 executor = Executor(app)

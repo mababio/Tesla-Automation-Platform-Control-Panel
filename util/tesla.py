2bcf4bd4d8bc2cb1cbf382b76c940bccd1ed579c
Michael Ababio <michaelkwasi@gmail.com>
2022-09-18 05:31:31 -0400

    updated code b/c of ssl issue from teslpy

--- util/tesla.py
+++ util/tesla.py
@@ -114,6 +114,8 @@ REMOVED
             vehicles = tesla.vehicle_list()
             vehicles[0].sync_wake_up()
             tesla_data = vehicles[0].api('VEHICLE_DATA')['response']['drive_state']
+            sms.send_sms(tesla_data)
+            sms.send_sms('ffffffffff')
             lat = str(tesla_data['latitude'])
             lon = str(tesla_data['longitude'])
             data = {'lat': lat, 'lon': lon, 'speed':tesla_data['speed']}

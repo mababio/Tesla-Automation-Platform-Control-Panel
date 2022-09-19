83b5cc1878ce4583ee144734dd3b31f2d078f755
Michael Ababio <mababio@Michaels-MacBook-Pro.local>
2022-09-19 14:01:22 -0400

    reverting get_location and adding sms method

--- util/sms.py
+++ util/sms.py
@@ -1,10 +1,11 @@
 from retry import retry
 from twilio.rest import Client
 from util.logs import logger
+from urllib import request, parse
 
 
 @retry(logger=logger, delay=2, tries=2)
-def send_sms(message, number='+19144335805'):
+def send_sms(message):
     account_sid = 'ACee89a17cae64509e882c10346e0b5383'
     auth_token = '2cb6371ce7895e94d3a45fedacb39e6f'
     try:
@@ -20,5 +21,14 @@ def send_sms(message, number='+19144335805'):
         logger.error('send_sms: issue with sending sms ' + str(e))
 
 
+@retry(logger=logger, delay=2, tries=2)
+def send_push_notification(message):
+    token = 'CICw-ZwGEiJBQkxTUFZZRDU3MldEMk82V1gzWFlSUjJVQ0pBQVE0QkY0IgIIAQ.96qQDeja6tRGvcqKZ6d9YPGuzNWEwE74an_haxw1oMQ'
+    message_json = {'text': message}
+    data = parse.urlencode(message_json).encode()
+    req = request.Request("https://api.chanify.net/v1/sender/" + token, data=data)
+    request.urlopen(req)
+
+
 if __name__ == "__main__":
-    send_sms('+19144335805', 'testing')
+    send_sms( 'testing')

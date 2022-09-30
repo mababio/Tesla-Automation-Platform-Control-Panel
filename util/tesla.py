ef2f85658338a8520363d262e209ab706e241729
Michael Ababio <michaelkwasi@gmail.com>
2022-09-30 14:15:11 -0400

    added delay for 2 mins to gcp scheduler

--- util/tesla.py
+++ util/tesla.py
@@ -93,11 +93,10 @@ class Tesla:
 
 
 if __name__ == "__main__":
-   print(settings['production']['URL']['foo'])
-   #  obj = Tesla()
+    obj = Tesla()
    #  # print(obj.get_location())
    #  # # print(obj.is_battery_good()) and self.is_parked
    #  # param_prox={'lat':40.669900, 'lon': -74.095629}
-   #  param_prox={'lat':40.670291, 'lon': -74.096888}
-   #  prox = requests.post('https://us-east4-ensure-dev-zone.cloudfunctions.net/function-tesla-prox', json=param_prox).json()
-   #  print(prox)
+    param_prox={'lat':40.663205, 'lon': -74.074595}
+    prox = requests.post('https://us-east4-ensure-dev-zone.cloudfunctions.net/function-tesla-prox', json=param_prox).json()
+    print(prox)

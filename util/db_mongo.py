8ea81e7813b84c3926eb9488fc510c5afaecd74c
Michael Ababio <michaelkwasi@gmail.com>
2022-10-04 17:01:58 -0400

    added TDD: garage.py, rm some functions, added more dbclient function

--- util/db_mongo.py
+++ util/db_mongo.py
@@ -1,6 +1,8 @@
 import json
 import pymongo
 from pymongo.server_api import ServerApi
+from util import notification
+from util import garage
 from util.logs import logger
 from google.cloud import pubsub_v1
 from config import settings
@@ -38,16 +40,25 @@ class DBClient:
         logger.debug('set_ifttt_trigger_lock::::: Updating ifttt_trigger_lock value to' + str(bol_val))
 
     def set_door_close_status(self, reason):
-        myquery = {"_id": "garage"}
-        new_values = {"$set": {"closed_reason": reason}}
-        self.tesla_database['garage'].update_one(myquery, new_values)
-        logger.debug('set_door_close_status::::: Updating door_close_status value to' + str(reason))
+        if isinstance(reason, garage.GarageCloseReason):
+            myquery = {"_id": "garage"}
+            new_values = {"$set": {"closed_reason": reason.value}}
+            self.tesla_database['garage'].update_one(myquery, new_values)
+            logger.debug('set_door_close_status::::: Updating door_close_status value to' + str(reason))
+        else:
+            logger.error('set_door_close_status::::: Issue with input given')
+            raise TypeError('set_door_close_status::::: GarageCloseReason Enum type was not provided')
 
     def set_door_open_status(self, reason):
-        myquery = {"_id": "garage"}
-        new_values = {"$set": {"opened_reason": reason}}
-        self.tesla_database['garage'].update_one(myquery, new_values)
-        logger.debug('set_door_open_status::::: Updating door_open_status value to' + str(reason))
+        if isinstance(reason, garage.GarageOpenReason):
+            myquery = {"_id": "garage"}
+            new_values = {"$set": {"opened_reason": reason.value}}
+            self.tesla_database['garage'].update_one(myquery, new_values)
+            logger.debug('set_door_open_status::::: Updating door_open_status value to' + str(reason))
+        else:
+            logger.error('set_door_open_status::::: Issue with input given')
+            raise TypeError('set_door_open_status::::: GarageOpenReason Enum type was not provided')
+
 
     def get_ifttt_trigger_lock(self):
         logger.debug('get_ifttt_trigger_lock::::: Getting ifttt_trigger_lock value')
@@ -59,10 +70,10 @@ class DBClient:
 
     def get_door_open_status(self):
         logger.debug('get_door_open_status::::: get_door_open_status: Getting door_open_status value')
-        return self.tesla_database['garage'].find_one()['opened_reason']
+        return self.tesla_database['garage'].find_one({'_id': 'garage'})['opened_reason']
 
     def get_saved_location(self):
-        return self.tesla_database['tesla_location'].find_one({'_id':'current'})
+        return self.tesla_database['tesla_location'].find_one({'_id': 'current'})
 
     def save_location(self, json_lat_lon):
         if isinstance(json_lat_lon, dict):
@@ -81,6 +92,20 @@ class DBClient:
     def climate_turned_on_via_automation_before(self):
         climate_turned_on_before = self.tesla_database['tesla_climate_status'].find_one({'_id': 'enum'})['climate_turned_on_before']
         return True if climate_turned_on_before == 'True' else False
+
+    def reset_climate_automation(self):
+        self.tesla_database['tesla_climate_status'].update_one({"_id": "enum"}, {"$set": {"climate_state": 'at_user_well'}})
+        self.tesla_database['tesla_climate_status'].update_one({"_id": "enum"}, {"$set": {"climate_turned_on_before": 'False'}})
+
+    def reset_all_flags_tap_is_complete(self):
+        self.set_ifttt_trigger_lock('False')
+        self.reset_climate_automation()
+        self.set_door_close_status(garage.GarageCloseReason.DRIVE_HOME)
+        if self.get_door_open_status() != garage.GarageOpenReason.DRIVE_HOME.value:
+            notification.send_push_notification('door open flag was not set correctly. it has been now')
+            self.set_door_open_status(garage.GarageOpenReason.DRIVE_HOME)
+
+
 
 
 if __name__ == "__main__":
--- util/garage.py
+++ util/garage.py
@@ -3,7 +3,6 @@ import requests
 from config import settings
 from util.logs import logger
 from util.notification import send_push_notification
-# import util.db_mongo as db_mongo
 
 
 class GarageCloseReason(Enum):
@@ -23,28 +22,13 @@ def garage_is_open():
                     == 'closed' else True
 
 
-def set_open_reason(garage_open_reason, db):
-    if isinstance(garage_open_reason, GarageOpenReason):
-        db.set_door_open_status(garage_open_reason.value)
-    else:
-        logger.error('set_open_reason::::: Issue with input given')
-        raise TypeError('set_open_reason::::: GarageOpenReason Enum type was not provided')
-
-
-def set_close_reason(garage_close_reason, db):
-    if isinstance(garage_close_reason, GarageCloseReason):
-        db.set_door_close_status(garage_close_reason.value)
-    else:
-        logger.error('set_close_reason::::: Issue with input given')
-        raise TypeError('set_close_reason::::: GarageCloseReason Enum type was not provided')
-
-
-def open_garage(db):
+def open_garage():
     send_push_notification('Would of opened the garage')
     return True
     try:
         return_val = requests.get(settings['development']['URL']['myq_garage'])
-        set_open_reason(GarageOpenReason.DRIVE_HOME, db)
+        ###set_open___reason(GarageOpenReason.DRIVE_HOME, db)     DONT USE THIS ...
+        # FUNCTION DOES NOT EXIST AND FLAG HAS BEEN SET IN trigger_tesla_home_automation
         return True if return_val else False
     except Exception as e:
         logger.error('open_garage::::: Issue with opening the garage::::: ' + str(e))
@@ -56,11 +40,4 @@ def close_garage():
     except Exception as e:
         logger.error('open_garage::::: Issue with opening the garage::::: ' + str(e))
 
-
-
-
-# db1 = db_mongo.DBClient()
-# set_close_reason(GarageCloseReason.DRIVE_AWAY, db1)
-
-#requests.post(settings['production']['URL']['myq_garage'], json={"state": 'close'}).json()
 

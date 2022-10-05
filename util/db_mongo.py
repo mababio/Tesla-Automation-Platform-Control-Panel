c580a315caf0ccbde6c1f075dbd3ba7131c9367e
Michael Ababio <michaelkwasi@gmail.com>
2022-10-05 10:18:29 -0400

    updated mongodb cred

--- util/db_mongo.py
+++ util/db_mongo.py
@@ -12,8 +12,9 @@ class DBClient:
 
     def __init__(self):
         self.publisher = pubsub_v1.PublisherClient()
-        self.tesla_gps_save_mongodb_topic = self.publisher.topic_path(settings['production']['pub_sub']['gps']['project']
-                                                                      , settings['production']['pub_sub']['gps']['topic'])
+        self.tesla_gps_save_mongodb_topic = self.publisher.topic_path(settings['production']['pub_sub']
+                                                                      ['gps']['project'],
+                                                                      settings['production']['pub_sub']['gps']['topic'])
         try:
             client = pymongo.MongoClient(settings['production']['mongo_client_url'], server_api=ServerApi('1'))
             self.tesla_database = client['tesla']
@@ -27,7 +28,11 @@ class DBClient:
         except Exception as e:
             logger.error("get_tesla_database::::: Error getting mongodb client conn to DB " + str(e))
 
-    def set_tesla_location_is_home_value(self,bol_value):
+    def get_tesla_location_is_home_value(self):
+        logger.debug('get_tesla_location_is_home_value::::: Getting is_home value')
+        return self.tesla_database['tesla_location'].find_one({"_id": "current"})['is_home']
+
+    def set_tesla_location_is_home_value(self, bol_value):
         myquery = {"_id": "current"}
         new_values = {"$set": {"is_home": bol_value}}
         self.tesla_database['tesla_location'].update_one(myquery, new_values)
@@ -59,7 +64,6 @@ class DBClient:
             logger.error('set_door_open_status::::: Issue with input given')
             raise TypeError('set_door_open_status::::: GarageOpenReason Enum type was not provided')
 
-
     def get_ifttt_trigger_lock(self):
         logger.debug('get_ifttt_trigger_lock::::: Getting ifttt_trigger_lock value')
         return self.tesla_database['tesla_trigger'].find_one()['lock']
@@ -90,12 +94,15 @@ class DBClient:
         return True if climate_state == 'climate_automation' else False
 
     def climate_turned_on_via_automation_before(self):
-        climate_turned_on_before = self.tesla_database['tesla_climate_status'].find_one({'_id': 'enum'})['climate_turned_on_before']
+        climate_turned_on_before = self.tesla_database['tesla_climate_status'].find_one({'_id': 'enum'})[
+            'climate_turned_on_before']
         return True if climate_turned_on_before == 'True' else False
 
     def reset_climate_automation(self):
-        self.tesla_database['tesla_climate_status'].update_one({"_id": "enum"}, {"$set": {"climate_state": 'at_user_well'}})
-        self.tesla_database['tesla_climate_status'].update_one({"_id": "enum"}, {"$set": {"climate_turned_on_before": 'False'}})
+        self.tesla_database['tesla_climate_status'].update_one({"_id": "enum"},
+                                                               {"$set": {"climate_state": 'at_user_well'}})
+        self.tesla_database['tesla_climate_status'].update_one({"_id": "enum"},
+                                                               {"$set": {"climate_turned_on_before": 'False'}})
 
     def reset_all_flags_tap_is_complete(self):
         self.set_ifttt_trigger_lock('False')
@@ -106,10 +113,5 @@ class DBClient:
             self.set_door_open_status(garage.GarageOpenReason.DRIVE_HOME)
 
 
-
-
 if __name__ == "__main__":
     obj = DBClient()
-    tesladb =  obj.get_tesla_database()
-    tesladb['tesla_climate_status'].find_one({'id':'enum'})['climate_state']
-    #print(obj.set_door_open_status("mababio"))

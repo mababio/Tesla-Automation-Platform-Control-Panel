531ad694e68a853c4fce56bed6cc03ad4f3e46bc
Michael Ababio <michaelkwasi@gmail.com>
2022-10-10 12:06:17 -0400

    enabled raspberry pi garage opener. fixed bug

--- util/garage.py
+++ util/garage.py
@@ -3,7 +3,6 @@ import requests
 from config import settings
 from util.logs import logger
 from util.notification import send_push_notification
-from util.db_mongo import DBClient
 
 
 class GarageCloseReason(Enum):
@@ -23,23 +22,7 @@ def garage_is_open():
                     == 'closed' else True
 
 
-def open_garage():
-    send_push_notification('Would of opened the garage')
-    return True
-    try:
-        DBClient().publish_open_garage()
-        #return_val = requests.get(settings['development']['URL']['myq_garage'])
-        ###set_open___reason(GarageOpenReason.DRIVE_HOME, db)     DONT USE THIS ...
-        # FUNCTION DOES NOT EXIST AND FLAG HAS BEEN SET IN trigger_tesla_home_automation
-        return True if return_val else False
-    except Exception as e:
-        logger.error('open_garage::::: Issue with opening the garage::::: ' + str(e))
 
 
-def close_garage():
-    try:
-        return requests.post(settings['production']['URL']['myq_garage'], json={"state": 'close'}).json()
-    except Exception as e:
-        logger.error('open_garage::::: Issue with opening the garage::::: ' + str(e))
 
 

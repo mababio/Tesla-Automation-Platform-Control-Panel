df7e68363628cba189f15ad6c2cf2d4e72814898
Michael Ababio <michaelkwasi@gmail.com>
2023-09-07 18:22:32 -0700

    removed sms method

--- util/notification.py
+++ util/notification.py
@@ -6,24 +6,6 @@ from config import settings
 
 
 @retry(logger=logger, delay=2, tries=2)
-def send_sms(message):
-    number=''
-REMOVED
-REMOVED
-    try:
-REMOVED
-        message = client.messages \
-            .create(
-                body=message,
-                from_='+12056548947',
-                to=number
-            )
-        return message.sid
-    except Exception as e:
-        logger.error('send_sms: issue with sending sms ' + str(e))
-
-
-@retry(logger=logger, delay=2, tries=2)
 def send_push_notification( message):
     publisher = pubsub_v1.PublisherClient()
     tesla_chanify_notification_topic = publisher.topic_path(settings['production']['pub_sub']
